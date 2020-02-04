08c546a0bdc69a5966f25f964280d4fdf88e123e
Jason Melton <jason.melton2@gmail.com>
2020-02-04 08:27:21 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,68 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+puts "Welcome to the Blackjack Table"
+
 end
 
 def deal_card
-  # code #deal_card here
+rand(10) + 1 
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+puts  "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+gets.chomp 
 end
 
-def end_game
-  # code #end_game here
+def end_game(num)
+puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  round = deal_card + deal_card
+  display_card_total(round)
+  round
 end
 
 def invalid_command
-  # code invalid_command here
+puts 'Please enter a valid command'
 end
+
+def hit?(current) 
+  prompt_user
+  input = get_user_input
+  if input == "s"
+    current
+  elsif input == "h"
+    current += deal_card 
+  else
+    invalid_command
+    hit?(current)
+  end 
+end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+welcome
+num = initial_round
+num2 = hit?(num)
+display_card_total(num2)
+if num2 < 21
+  hit?
+else 
+  end_game(num2)
+end 
+
 end
     
